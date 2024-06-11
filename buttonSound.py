8a7a4b5efa2dbaf024a33672c0d48d9c6d7411d3
Annie Hua <annihua96@gmail.com>
2024-06-11 18:52:37 -0400

    add sound for red

--- buttonSound.py
+++ buttonSound.py
@@ -1,3 +1,4 @@
+from pygame import mixer 
 import RPi.GPIO as GPIO
 
 redButton = 22 
@@ -6,24 +7,28 @@ blueButton = 16
 
 
 def setup():
+    mixer.init()
     GPIO.setmode(GPIO.BOARD) # use PHYSICAL GPIO Numbering
     # GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
     GPIO.setup(redButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
     GPIO.setup(yellowButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
     GPIO.setup(blueButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
 
+# def setLanguage():
+#     redSound = '/sounds/English/red.m4a'
 
 def loop():
     while True:
-        print('looping')
         if GPIO.input(redButton)==GPIO.LOW: # if button is pressed
-            print ('button pressed >>>') # print information on terminal
+            print ('Red Button: button pressed >>>') # print information on terminal
+            mixer.music.load('/sounds/English/red.m4a')
+            mixer.music.play()
         if GPIO.input(yellowButton)==GPIO.LOW: # if button is pressed
-            print ('button pressed >>>') # print information on terminal
+            print ('Yellow Button: pressed >>>') # print information on terminal
         if GPIO.input(blueButton)==GPIO.LOW: # if button is pressed
-            print ('button pressed >>>') # print information on terminal
-    else : # if button is released
-        print ('button released <<<')
+            print ('Blue Button: pressed >>>') # print information on terminal
+    # else : # if button is released
+    #     print ('button released <<<')
 def hello():
     print('Hello')
 
