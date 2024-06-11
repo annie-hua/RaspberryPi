155daa83972e23045d01af15d2663d26e9574c4d
Annie Hua <annihua96@gmail.com>
2024-06-11 14:53:37 -0400

    led remove

--- buttonSound.py
+++ buttonSound.py
@@ -7,21 +7,23 @@ blueButton = 17
 
 def setup():
     GPIO.setmode(GPIO.BOARD) # use PHYSICAL GPIO Numbering
-    GPIO.setup(ledPin, GPIO.OUT) # set ledPin to OUTPUT mode
-    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
+    # GPIO.setup(ledPin, GPIO.OUT) # set ledPin to OUTPUT mode
+    GPIO.setup(redButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
+    GPIO.setup(yellowButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
+    GPIO.setup(blueButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
+
 
 def loop():
     while True:
         if GPIO.input(redButton)==GPIO.LOW: # if button is pressed
-                print ('led turned on >>>') # print information on terminal
+            print ('button pressed >>>') # print information on terminal
     else : # if button is released
-        GPIO.output(ledPin,GPIO.LOW) # turn off led
-        print ('led turned off <<<')
+        print ('button released <<<')
 def hello():
     print('Hello')
 
 def destroy():
-    GPIO.output(ledPin, GPIO.LOW)     # turn off led 
+    # GPIO.output(ledPin, GPIO.LOW)     # turn off led 
     GPIO.cleanup()                    # Release GPIO resource
 
 if __name__ == '__main__':     # Program entrance
