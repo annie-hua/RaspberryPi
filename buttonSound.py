bebf364a138f0ba30e687a4e90aaaaf930eb3146
Annie Hua <annihua96@gmail.com>
2024-06-11 23:31:25 -0400

    mp3 files

--- buttonSound.py
+++ buttonSound.py
@@ -1,10 +1,12 @@
 from pygame import mixer 
 import RPi.GPIO as GPIO
+from languageSoundMap import languageSoundMap
 
 redButton = 22 
 yellowButton = 18 
 blueButton = 16
 
+language = 'English'
 
 def setup():
     mixer.init()
@@ -14,21 +16,19 @@ def setup():
     GPIO.setup(yellowButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
     GPIO.setup(blueButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set buttonPin to PULL UP INPUT>
 
-# def setLanguage():
-#     redSound = '/sounds/English/red.m4a'
-
 def loop():
     while True:
         if GPIO.input(redButton)==GPIO.LOW: # if button is pressed
             print ('Red Button: button pressed >>>') # print information on terminal
-            mixer.music.load('./sounds/English/red.m4a')
-            mixer.music.play()
+            mixer.music.load(languageSoundMap.language['red'])
         if GPIO.input(yellowButton)==GPIO.LOW: # if button is pressed
             print ('Yellow Button: pressed >>>') # print information on terminal
+            mixer.music.load(languageSoundMap.language['yellowButton'])
         if GPIO.input(blueButton)==GPIO.LOW: # if button is pressed
             print ('Blue Button: pressed >>>') # print information on terminal
-            mixer.music.load('./sounds/English/blue.mp3')
-            mixer.music.play()
+            mixer.music.load(languageSoundMap.language['blueButton'])
+        mixer.music.play()
+
     # else : # if button is released
     #     print ('button released <<<')
 def hello():
---
+++ languageSoundMap.py
@@ -0,0 +1,7 @@
+def generate_language_sound_map(languages, colors):
+    return {language: {color: f'./sounds/{language}/{color}.mp3' for color in colors} for language in languages}
+
+languages = ['English', 'Spanish', 'Cantonese']
+colors = ['red', 'yellow', 'blue']
+
+languageSoundMap = generate_language_sound_map(languages, colors)
