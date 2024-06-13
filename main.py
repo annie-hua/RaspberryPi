bb0aed893136cf77575f2c3ccefbeb8696ff052a
Annie Hua <annihua96@gmail.com>
2024-06-13 12:03:28 -0400

    update main

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
     buttonSound = ButtonSound()
     buttonSound.setup()
-    try:
-        buttonSound.loop()
-    except KeyboardInterrupt:  # Press ctrl-c to end the program.
-        destroy()   
-
     output_queue = multiprocessing.Queue()
     ble_process = BLEProcess(output_queue)
     ble_process.start()
+    try:
+        buttonSound.loop()
+        while True:
+            try:
+                curr_value = output_queue.get(timeout=1)
+                print(f"Value written to Characteristic with UUID {curr_value['uuid']}: {curr_value['value']}")
+                newLangage = curr_value['value']
+                if curr_value:
+                    buttonSound.setLanguage(newLangage)
+            except queue.Empty:
+                time.sleep(1)
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        buttonSound.destroy()   
 
-    while True:
-        try:
-            curr_value = output_queue.get(timeout=1)
-            print(f"Value written to Characteristic with UUID {curr_value['uuid']}: {curr_value['value']}")
-            newLangage = curr_value['value']
-            if curr_value:
-                buttonSound.setLanguage(newLangage)
-        except queue.Empty:
-            time.sleep(1)
+    
