72e4a34484bdfb8ba161be676af466439407ca32
Annie Hua <annihua96@gmail.com>
2024-06-27 21:53:56 -0400

    sound source

--- languageSoundMap.py
+++ languageSoundMap.py
@@ -1,7 +1,26 @@
 def generate_language_sound_map(languages, colors):
-    return {language: {color: f'./sounds/{language}/{color}.mp3' for color in colors} for language in languages}
+    return {language: {color: f'./audio/{language}/color/{color}.mp3' for color in colors} for language in languages}
 
-languages = ['English', 'Spanish', 'Cantonese']
-colors = ['red', 'yellow', 'blue', 'green', 'orange']
+languages = [
+    'English',
+    'Spanish',
+    'Chinese',
+    'Filipino', #Tagalog
+    'Vietnamese',
+    'Arabic',
+    'Korean',
+    'Hindi',
+    'Russian',
+    'German',
+    'Portuguese',
+    'Italian',
+    'Urdu',
+    'Telugu',
+    'Persian',
+    'Gujarati',
+    'Hungarian',
+    'Cantonese'
+]
+colors = ['red', 'yellow', 'blue', 'green', 'orange', 'black', 'white', 'gray', 'brown', 'pink', 'purple']
 
 languageSoundMap = generate_language_sound_map(languages, colors)
